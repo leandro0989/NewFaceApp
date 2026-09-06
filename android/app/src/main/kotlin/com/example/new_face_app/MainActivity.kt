3570ab3544aa47970f3409ce2ac928a1f568f144
leandro0989 <leandro27092010@gmail.com>
2026-09-06 00:31:23 -0400

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/new_face_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/new_face_app/MainActivity.kt
@@ -1,6 +1,37 @@
-package com.example.new_face_app
+<manifest xmlns:android="http://schemas.android.com/apk/res/android">
+    <application
+       android:label="New Face App"
+       android:name="${applicationName}"
+       android:icon="@mipmap/ic_launcher">
+       
+        <meta-data
+            android:name="flutterEmbedding"
+            android:value="2" />
 
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
+        <activity
+            android:name=".MainActivity"
+            android:exported="true"
+            android:launchMode="singleTop"
+            android:taskAffinity=""
+            android:theme="@style/LaunchTheme"
+            android:configChanges="orientation|keyboardHidden|keyboard|screenSize|smallestScreenSize|locale|layoutDirection|fontScale|screenLayout|density|uiMode"
+            android:hardwareAccelerated="true"
+            android:windowSoftInputMode="adjustResize">
+            
+            <meta-data
+              android:name="io.flutter.embedding.android.NormalTheme"
+              android:resource="@style/NormalTheme"
+              />
+            <intent-filter>
+                <action android:name="android.intent.action.MAIN"/>
+                <category android:name="android.intent.category.LAUNCHER"/>
+            </intent-filter>
+        </activity>
+        
+        <meta-data
+            android:name="flutterEmbedding"
+            android:value="2" />
+            
+    </application>
+</manifest>
+  
